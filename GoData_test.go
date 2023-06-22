84642d5dbf5daebb3ac750fcbb31821e5a0f523d
Carlos Fitl <fitlcarlos@gmail.com>
2023-06-22 08:20:17 -0400

    Update GoData_test.go

--- GoData_test.go
+++ GoData_test.go
@@ -5,35 +5,35 @@ import (
 	"testing"
 )
 
-//func TestGodata(t *testing.T) {
-//	connectStr := "oracle://nbsama:new@100.0.65.224:1521/fab"
-//
-//	db, err := NewConnection(DialectType(ORACLE), connectStr)
-//
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	defer db.Close()
-//
-//	ds := NewDataSet(db)
-//	err = ds.
-//		AddSql("SELECT DESCRICAO FROM FAB_PROCESSO").
-//		AddSql("WHERE ID BETWEEN :idini AND :idfim").
-//		SetInputParam("idini", 20).
-//		SetInputParam("idfim", 100).
-//		Open()
-//
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//
-//	ds.First()
-//	for !ds.Eof() {
-//		t.Log(ds.FieldByName("DESCRICAO").AsString())
-//		ds.Next()
-//	}
-//}
+func TestGodata(t *testing.T) {
+	connectStr := "oracle://nbsama:new@100.0.65.224:1521/fab"
+
+	db, err := NewConnection(DialectType(ORACLE), connectStr)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
+
+	ds := NewDataSet(db)
+	err = ds.
+		AddSql("SELECT DESCRICAO FROM FAB_PROCESSO").
+		AddSql("WHERE ID BETWEEN :idini AND :idfim").
+		SetInputParam("idini", 20).
+		SetInputParam("idfim", 100).
+		Open()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ds.First()
+	for !ds.Eof() {
+		t.Log(ds.FieldByName("DESCRICAO").AsString())
+		ds.Next()
+	}
+}
 
 func TestDataSetToStruct(t *testing.T) {
 
