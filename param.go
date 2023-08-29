c3f98ac1f0e518683ef0bbf836d582b5cc4eaee6
fitlcarlos <fitlcarlos@gmail.com>
2023-08-29 15:14:00 -0400

    Fix struct param

--- param.go
+++ param.go
@@ -27,7 +27,11 @@ func NewParam(paramName string, paramType ParamType) *Param {
 		ParamType: paramType,
 	}
 }
-func (param Param) AsValue() *Variant {
+func (param *Param) AsValue() *Variant {
+	if param.Value.Value == nil {
+		return param.Value
+	}
+
 	if IsPointer(param.Value.Value) {
 		a := reflect.ValueOf(param.Value.Value).Elem().Interface()
 		return &Variant{Value: a}
@@ -36,42 +40,42 @@ func (param Param) AsValue() *Variant {
 	}
 }
 
-func (param Param) AsString() string {
+func (param *Param) AsString() string {
 	return param.AsValue().AsString()
 }
 
-func (param Param) AsInt() int {
+func (param *Param) AsInt() int {
 	return param.AsValue().AsInt()
 }
 
-func (param Param) AsInt8() int8 {
+func (param *Param) AsInt8() int8 {
 	return param.AsValue().AsInt8()
 }
 
-func (param Param) AsInt16() int16 {
+func (param *Param) AsInt16() int16 {
 	return param.AsValue().AsInt16()
 }
 
-func (param Param) AsInt32() int32 {
+func (param *Param) AsInt32() int32 {
 	return param.AsValue().AsInt32()
 }
 
-func (param Param) AsInt64() int64 {
+func (param *Param) AsInt64() int64 {
 	return param.AsValue().AsInt64()
 }
 
-func (param Param) AsFloat() float32 {
+func (param *Param) AsFloat() float32 {
 	return param.AsValue().AsFloat()
 }
 
-func (param Param) AsFloat64() float64 {
+func (param *Param) AsFloat64() float64 {
 	return param.AsValue().AsFloat64()
 }
 
-func (param Param) AsBool() bool {
+func (param *Param) AsBool() bool {
 	return param.AsValue().AsBool()
 }
 
-func (param Param) AsDateTime() time.Time {
+func (param *Param) AsDateTime() time.Time {
 	return param.AsValue().AsDateTime()
 }
